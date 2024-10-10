8c2c75a2019b7380ae7c9f055edf5cf708b120c2
Nayana <nayanamr2004@gmail.com>
2024-10-10 15:05:01 +0530

    10/10/24

---
+++ Ninth-project/src/App.jsx
@@ -0,0 +1,14 @@
+import { useRef } from "react"
+const App = () => {
+
+  const inputRef = useRef(null);
+  return (
+    <div>
+      <input ref={inputRef} type="text"/>
+      <button onClick={()=>{console.log(inputRef.current.value);}}>Submit</button>
+    </div>
+  )
+}
+
+export default App
+
